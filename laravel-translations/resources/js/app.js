f212f32ad4309bd91cb0ed74da6c19bb3a5fe947
Shahghasi Adil <shaghasY555@gmail.com>
2023-01-25 23:04:00 +0430

    dir deleted

--- laravel-translations/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import './modal';
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
-
-Alpine.plugin(focus)
-
-window.Alpine = Alpine;
-
-Alpine.start();
--- laravel-translations/tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Outhebox\LaravelTranslations\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
