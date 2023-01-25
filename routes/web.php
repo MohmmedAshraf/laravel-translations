9ba18138bb50915ca7f7aa8fb53b6a4abc5db407
Shahghasi Adil <shaghasY555@gmail.com>
2023-01-25 15:17:34 +0430

    Routes enhancement and shorter level

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Outhebox\LaravelTranslations\Http\Controllers\TranslationController;
 
-Route::get('/', [TranslationController::class, 'index'])->name('translations_ui.index');
+Route::controller(TranslationController::class)
+    ->as('translations_ui.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
 
-Route::prefix('phrases')->group(function () {
-    Route::get('{translation}', [TranslationController::class, 'phrases'])->name('translations_ui.phrases.index');
-    Route::get('{translation}/edit/{phrase:uuid}', [TranslationController::class, 'phrase'])->name('translations_ui.phrases.show');
-});
+        Route::prefix('phrases')
+            ->as('phrases.')
+            ->group(function () {
+                Route::get('{translation}', 'phrases')->name('index');
+                Route::get('{translation}/edit/{phrase:uuid}', 'phrase')->name('show');
+            });
+    });
