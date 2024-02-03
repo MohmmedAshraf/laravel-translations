21e8bcb10398f3f271b83e66f7784c171f396227
Mohamed Ashraf <cupo.ashraf@gmail.com>
2024-02-03 13:06:24 +0400

    Fix code styles

--- src/helpers.php
+++ src/helpers.php
@@ -62,6 +62,7 @@ if (! function_exists('buildPhrasesTree')) {
         foreach ($phrases as $phrase) {
             if ($phrase->file->file_name === "$locale.json") {
                 $tree[$locale][$phrase->file->file_name][$phrase->key] = ! blank($phrase->value) ? $phrase->value : $phrase->source->value;
+
                 continue;
             }
             setArrayValue(
