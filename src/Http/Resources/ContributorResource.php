f2c1a73b33e69102c13f0dd1ae58ac765673e4aa
Brian Henry <BrianHenryIE@gmail.com>
2025-02-05 20:28:13 -0800

    Add `UnwrappedAnonymousResourceCollection` (#149)

--- src/Http/Resources/ContributorResource.php
+++ src/Http/Resources/ContributorResource.php
@@ -3,6 +3,7 @@
 namespace Outhebox\TranslationsUI\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
 use Outhebox\TranslationsUI\Models\Contributor;
 
 /** @mixin Contributor */
@@ -24,5 +25,18 @@ class ContributorResource extends JsonResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
+    }
+
+    /**
+     * Create an AnonymousResourceCollection without wrapping
+     *
+     * @see JsonResource::newCollection()
+     *
+     * @param  mixed|Collection  $resource
+     * @return UnwrappedAnonymousResourceCollection
+     */
+    protected static function newCollection($resource)
+    {
+        return new UnwrappedAnonymousResourceCollection($resource, ContributorResource::class);
     }
 }
---
+++ src/Http/Resources/UnwrappedAnonymousResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Outhebox\TranslationsUI\Http\Resources;
+
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UnwrappedAnonymousResourceCollection extends AnonymousResourceCollection
+{
+    public static $wrap = null;
+}
